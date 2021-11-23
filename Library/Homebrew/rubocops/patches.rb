1b8574c5a6a7ff1555702c0426e5c22e8817d789
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-23 00:37:53 -0800

    style: report issue if gitweb patch url contains `a=patch`.
    `a=commitdiff_plain` should be used instead.

--- Library/Homebrew/rubocops/patches.rb
+++ Library/Homebrew/rubocops/patches.rb
@@ -43,60 +43,100 @@ module RuboCop
 
         def patch_problems(patch_url_node)
           patch_url = string_content(patch_url_node)
+          report_github_unstable_pr(patch_url_node, patch_url)
+          report_gitlab_unstable_mr(patch_url_node, patch_url)
+          report_github_wrong_extension(patch_url_node, patch_url)
+          report_gitlab_wrong_extension(patch_url_node, patch_url)
+          report_github_missing_full_index(patch_url_node, patch_url)
+          report_github_missing_revision(patch_url_node, patch_url)
+          report_github_patch_diff(patch_url_node, patch_url)
+          report_macports_trunk(patch_url_node, patch_url)
+          report_macports_trac_http(patch_url_node, patch_url)
+          report_debian_http(patch_url_node, patch_url)
+          report_gitweb_patch(patch_url_node, patch_url)
+        end
 
-          if regex_match_group(patch_url_node, %r{https://github.com/[^/]*/[^/]*/pull})
-            problem "Use a commit hash URL rather than an unstable pull request URL: #{patch_url}"
-          end
+        def report_github_unstable_pr(patch_url_node, patch_url)
+          return unless regex_match_group(patch_url_node, %r{https://github.com/[^/]*/[^/]*/pull})
 
-          if regex_match_group(patch_url_node, %r{.*gitlab.*/merge_request.*})
-            problem "Use a commit hash URL rather than an unstable merge request URL: #{patch_url}"
-          end
+          problem "Use a commit hash URL rather than an unstable pull request URL: #{patch_url}"
+        end
 
-          if regex_match_group(patch_url_node, %r{https://github.com/[^/]*/[^/]*/commit/[a-fA-F0-9]*\.diff})
-            problem "GitHub patches should end with .patch, not .diff: #{patch_url}"
-          end
+        def report_gitlab_unstable_mr(patch_url_node, patch_url)
+          return unless regex_match_group(patch_url_node, %r{.*gitlab.*/merge_request.*})
 
+          problem "Use a commit hash URL rather than an unstable merge request URL: #{patch_url}"
+        end
+
+        def report_github_wrong_extension(patch_url_node, patch_url)
+          return unless regex_match_group(patch_url_node, %r{https://github.com/[^/]*/[^/]*/commit/[a-fA-F0-9]*\.diff})
+
+          problem "GitHub patches should end with .patch, not .diff: #{patch_url}"
+        end
+
+        def report_gitlab_wrong_extension(patch_url_node, patch_url)
           # Only .diff passes `--full-index` to `git diff` and there is no documented way
           # to get .patch to behave the same for GitLab.
-          if regex_match_group(patch_url_node, %r{.*gitlab.*/commit/[a-fA-F0-9]*\.patch})
-            problem "GitLab patches should end with .diff, not .patch: #{patch_url}"
-          end
+          return unless regex_match_group(patch_url_node, %r{.*gitlab.*/commit/[a-fA-F0-9]*\.patch})
 
+          problem "GitLab patches should end with .diff, not .patch: #{patch_url}"
+        end
+
+        def report_github_missing_full_index(patch_url_node, patch_url)
           gh_patch_param_pattern = %r{https?://github\.com/.+/.+/(?:commit|pull)/[a-fA-F0-9]*.(?:patch|diff)}
-          if regex_match_group(patch_url_node, gh_patch_param_pattern) && !patch_url.match?(/\?full_index=\w+$/)
-            problem "GitHub patches should use the full_index parameter: #{patch_url}?full_index=1"
-          end
+          return unless regex_match_group(patch_url_node, gh_patch_param_pattern) && !patch_url.match?(/\?full_index=\w+$/)
 
+          problem "GitHub patches should use the full_index parameter: #{patch_url}?full_index=1"
+        end
+
+        def report_github_missing_revision(patch_url_node, patch_url)
           gh_patch_patterns = Regexp.union([%r{/raw\.github\.com/},
                                             %r{/raw\.githubusercontent\.com/},
                                             %r{gist\.github\.com/raw},
                                             %r{gist\.github\.com/.+/raw},
                                             %r{gist\.githubusercontent\.com/.+/raw}])
-          if regex_match_group(patch_url_node, gh_patch_patterns) && !patch_url.match?(%r{/[a-fA-F0-9]{6,40}/})
-            problem "GitHub/Gist patches should specify a revision: #{patch_url}"
-          end
+          return unless regex_match_group(patch_url_node, gh_patch_patterns) && !patch_url.match?(%r{/[a-fA-F0-9]{6,40}/})
 
+          problem "GitHub/Gist patches should specify a revision: #{patch_url}"
+        end
+
+        def report_github_patch_diff(patch_url_node, patch_url)
           gh_patch_diff_pattern =
             %r{https?://patch-diff\.githubusercontent\.com/raw/(.+)/(.+)/pull/(.+)\.(?:diff|patch)}
-          if regex_match_group(patch_url_node, gh_patch_diff_pattern)
-            problem "Use a commit hash URL rather than patch-diff: #{patch_url}"
-          end
+          return unless regex_match_group(patch_url_node, gh_patch_diff_pattern)
 
-          if regex_match_group(patch_url_node, %r{macports/trunk})
-            problem "MacPorts patches should specify a revision instead of trunk: #{patch_url}"
-          end
+          problem "Use a commit hash URL rather than patch-diff: #{patch_url}"
+        end
 
-          if regex_match_group(patch_url_node, %r{^http://trac\.macports\.org})
-            problem "Patches from MacPorts Trac should be https://, not http: #{patch_url}" do |corrector|
-              correct = patch_url_node.source.gsub(%r{^http://}, "https://")
-              corrector.replace(patch_url_node.source_range, correct)
-            end
-          end
+        def report_macports_trunk(patch_url_node, patch_url)
+          return unless regex_match_group(patch_url_node, %r{macports/trunk})
 
+          problem "MacPorts patches should specify a revision instead of trunk: #{patch_url}"
+        end
+
+        def report_macports_trac_http(patch_url_node, patch_url)
+          return unless regex_match_group(patch_url_node, %r{^http://trac\.macports\.org})
+
+          problem "Patches from MacPorts Trac should be https://, not http: #{patch_url}" do |corrector|
+            correct = patch_url_node.source.gsub(%r{^http://}, "https://")
+            corrector.replace(patch_url_node.source_range, correct)
+          end
+        end
+
+        def report_debian_http(patch_url_node, patch_url)
           return unless regex_match_group(patch_url_node, %r{^http://bugs\.debian\.org})
 
           problem "Patches from Debian should be https://, not http: #{patch_url}" do |corrector|
             correct = patch_url_node.source.gsub(%r{^http://}, "https://")
+            corrector.replace(patch_url_node.source_range, correct)
+          end
+        end
+
+        def report_gitweb_patch(patch_url_node, patch_url)
+          return unless regex_match_group(patch_url_node, %r{/\?p=.+;a=patch;h=.+})
+
+          problem "Use a=commitdiff_plain rather than a=patch: #{patch_url}" do |corrector|
+            correct = patch_url_node.source.sub(";a=patch;", ";a=commitdiff_plain;")
             corrector.replace(patch_url_node.source_range, correct)
           end
         end
